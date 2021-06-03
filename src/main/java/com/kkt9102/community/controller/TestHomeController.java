183681a8cf58c434b063f092877a0d74e100d671
kkt9102 <63387875+kkt9102@users.noreply.github.com>
2021-06-03 20:22:06 +0900

    Add files via upload
    test commit

---
+++ src/main/java/com/kkt9102/community/controller/TestHomeController.java
@@ -0,0 +1,5 @@
+package com.kkt9102.community.controller;
+
+public class TestHomeController {
+
+}
---
+++ src/test/java/com/kkt9102/community/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkt9102.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
